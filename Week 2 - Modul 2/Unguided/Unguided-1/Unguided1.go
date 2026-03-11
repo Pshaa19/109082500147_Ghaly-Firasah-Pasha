154e9ffb2050a26cc0c9d2c8c37fb639e2b690d2
Pshaa19 <ghalypasha62@gmail.com>
2026-03-11 23:08:09 +0700

    tes commit

--- Week 2 - Modul 2/Unguided/Unguided-1/Unguided1.go
+++ Week 2 - Modul 2/Unguided/Unguided-1/Unguided1.go
@@ -2,20 +2,13 @@ package main
 
 import "fmt"
 
-func cekKabisat(tahun int) bool {
-	if (tahun%400 == 0) || (tahun%4 == 0 && tahun%100 != 0) {
-		return true
-	}
-	return false
-}
-
 func main() {
 	var tahun int
 
 	fmt.Print("Tahun : ")
-	fmt.Scanln(&tahun)
+	fmt.Scan(&tahun)
 
-	isKabisat := cekKabisat(tahun)
+	isKabisat := (tahun%400 == 0) || (tahun%4 == 0 && tahun%100 != 0)
 
 	if isKabisat {
 		fmt.Println("Kabisat : True")
--- Week 2 - Modul 2/Unguided/Unguided-3/Unguided3.go
+++ Week 2 - Modul 2/Unguided/Unguided-3/Unguided3.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var totalBeratGram int
+
+	fmt.Print("Masukkan total berat (gram): ")
+	fmt.Scan(&totalBeratGram)
+
+	kg := totalBeratGram / 1000
+	sisaGram := totalBeratGram % 1000
+
+	biayaKg := kg * 10000
+	biayaSisa := 0
+
+	if totalBeratGram > 10000 {
+		biayaSisa = 0
+	} else if sisaGram >= 500 {
+		biayaSisa = sisaGram * 5
+	} else if sisaGram > 0 {
+		biayaSisa = sisaGram * 15
+	}
+
+	totalBiaya := biayaKg + biayaSisa
+
+	fmt.Println("\n===== Detail Perhitungan =====")
+	fmt.Printf("Detail berat : %d kg + %d gram\n", kg, sisaGram)
+	fmt.Printf("Detail biaya : Rp. %d + Rp. %d\n", biayaKg, biayaSisa)
+	fmt.Printf("Total biaya: Rp %d\n", totalBiaya)
 }
